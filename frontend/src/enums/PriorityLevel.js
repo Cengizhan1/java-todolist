fd35e17ebd95e7503135b3ad701bf09e5a97b19f
Cengizhan <182803014@ogr.cbu.edu.tr>
2023-09-15 00:26:38 +0300

    frontend/ klasörünü .gitignore'dan kaldır

--- frontend/src/enums/PriorityLevel.js
+++
@@ -1,8 +0,0 @@
-// Öncelik seviyeleri için enum
-const PriorityLevel = {
-    LOW: 'LOW',
-    MEDIUM: 'MEDIUM',
-    HIGH: 'HIGH',
-};
-
-export default PriorityLevel;
--- frontend/src/enums/TaskState.js
+++
@@ -1,8 +0,0 @@
-// Task durumu için enum
-const TaskStateEnum = {
-    TODO: 'TODO',
-    IN_PROGRESS: 'IN_PROGRESS',
-    DONE: 'DONE',
-};
-
-export default TaskStateEnum;
